413e84716e486b51d5dafa0ad2bb09a47ad05d57
davlatabdullaev <davlatdavlat0206@gmail.com>
2023-11-18 02:04:40 +0500

    lesson 4 home works

---
+++ lesson4.go
@@ -0,0 +1,55 @@
+package main
+
+func main() {
+
+	// 1-masala
+	/*
+		fmt.Println("To'g'ri to'rtburchakning a tomonini kiriting ")
+		var a int
+		fmt.Scan(&a)
+
+		fmt.Println("To'g'ri to'rtburchakning b tomonini kiriting ")
+		var b int
+		fmt.Scan(&b)
+
+		fmt.Println("To'g'ri to'rtburchakning yuzi ", (a * b))
+	*/
+
+	// 2-masala
+	/*
+		fmt.Println("a ni kiriting ")
+		var a int
+		fmt.Scan(&a)
+		fmt.Println(" a ni kubi ", (a * a * a))
+	*/
+
+	// 3-masala
+	/*
+		fmt.Println("Doira radiusini kiriting ")
+		var r float32
+		fmt.Scan(&r)
+		const pi float32 = 3.14
+		fmt.Println(" Doira radiusi ", (pi * r * r))
+	*/
+	// 4-masala
+	/*
+		fmt.Println("To'g'ri to'rtburchakning a tomonini kiriting ")
+		var a int
+		fmt.Scan(&a)
+
+		fmt.Println("To'g'ri to'rtburchakning b tomonini kiriting ")
+		var b int
+		fmt.Scan(&b)
+
+		fmt.Println("To'g'ri to'rtburchakning perimetri ", (2 * (a + b)))
+	*/
+	// 5-masala
+	/*
+		fmt.Println("Shar radiusini kiriting ")
+		var r float32
+		fmt.Scan(&r)
+		const pi float32 = 3.14
+		fmt.Println("To'la sirti S = ", (4 * pi * r * r))
+		fmt.Println("Shar hajmi  V = ", ((4.0 / 3.0) * pi * r * r * r))
+	*/
+}
