195308af88c6116ae8fc9530436423d8c3c20ebf
davlatabdullaev <davlatdavlat0206@gmail.com>
2023-11-18 01:25:32 +0500

    lesson 3 home work

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+func main() {
+
+	// 1-masala
+	/*
+		fmt.Println("Uch xonali son kiriting ")
+		var son int
+		fmt.Scan(&son)
+		var (
+			yuzlik int = son / 100
+			onlik  int = (son % 100) / 10
+			birlik int = son % 10
+		)
+		var yangison int = yuzlik*100 + birlik*10 + onlik
+		fmt.Println(yangison)
+	*/
+
+	// 2-masala
+	/*
+		fmt.Println("999 dan katta son kiriting ")
+		var son int
+		fmt.Scan(&son)
+		fmt.Println("Yuzlar xonasidagi raqam ", ((son)/100)%10)
+	*/
+
+	// 3-masala
+	/*
+		fmt.Println("999 dan katta son kiriting ")
+		var son int
+		fmt.Scan(&son)
+		fmt.Println("Minglar xonasidagi raqam ", ((son)/1000)%10)
+	*/
+
+	// 4-masala
+	/*
+		fmt.Println("Sekundni kiriting ")
+		var sekund int
+		fmt.Scan(&sekund)
+		var minut = sekund / 60
+		fmt.Print(minut, " minut to'liq otdi")
+	*/
+	// 5-masala
+	/*
+		fmt.Println("Sekundni kiriting ")
+		var sekund int
+		fmt.Scan(&sekund)
+		var soat = sekund / 3600
+		fmt.Println(soat, " soat toliq otdi")
+	*/
+
+	// 6-masala
+	/*
+		fmt.Println("Sekundni kiriting ")
+		var sekund int
+		fmt.Scan(&sekund)
+		var minut = sekund / 60
+		fmt.Print(minut, " minut ")
+		fmt.Println(sekund-minut*60, " sekund")
+	*/
+	// 7-masala
+	/*
+		fmt.Println("Sekundni kiriting ")
+		var sekund int
+		fmt.Scan(&sekund)
+		var soat int = sekund / 3600
+		var qsekund int = (sekund - soat) / 60
+		fmt.Print(soat, " soat ")
+		fmt.Println(qsekund, " sekund")
+	*/
+
+	// 8-masala
+	/*
+		fmt.Println("Sekundni kiriting ")
+		var sekund int
+		fmt.Scan(&sekund)
+		var soat int = sekund / 3600
+		var minut int = (sekund / 60) % 60
+		var qsekund int = (sekund % 60) % 60
+		fmt.Println(soat, " soat ", minut, " minut ", qsekund, " sekund ")
+	*/
+	// 9-masala
+	/*
+		fmt.Println("999 dan katta son kiriting ")
+		var son int
+		fmt.Scan(&son)
+		fmt.Println("Minglar xonasidagi raqam ", ((son)/1000)%10)
+	*/
+}
